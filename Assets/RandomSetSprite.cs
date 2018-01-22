a27f0e5ead4fede4e38394cadf035142691986e2
MSI\willi <excalibran@gmail.com>
2018-01-21 18:11:39 -0600

    generic civilians ready

---
+++ Assets/RandomSetSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSetSprite : MonoBehaviour {
+
+  public List<Sprite> spriteSet;
+  SpriteRenderer render;
+
+	// Use this for initialization
+	void Start () {
+
+    render = GetComponent<SpriteRenderer>();
+
+    if (spriteSet == null)
+    {
+      Debug.Log("Missing random sprites");
+    }
+    else {
+      render.sprite = spriteSet[Random.Range(0, spriteSet.Count-1)];
+    }
+	}
+	
+	
+}
